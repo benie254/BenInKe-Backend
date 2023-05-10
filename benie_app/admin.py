b3e397ab7af458864c5f8dea9f7241978cda85c8
benie254 <davinci.monalissa@gmail.com>
2023-05-10 11:33:10 +0300

    remove unused imports

--- benie_app/admin.py
+++ benie_app/admin.py
@@ -6,7 +6,6 @@ from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import (
     AdminPasswordChangeForm
     )
-from django.contrib.auth.models import Group, User 
 from django.core.exceptions import PermissionDenied
 from django.db import router, transaction 
 from django.http import Http404, HttpResponseRedirect
